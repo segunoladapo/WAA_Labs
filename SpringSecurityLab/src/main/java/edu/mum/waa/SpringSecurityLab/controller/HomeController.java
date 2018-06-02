01e49584868afce1a7ddc14f6839a89faa4f2e9e
segun <segxy4show2000@gmail.com>
2018-06-02 06:38:52 -0500

    welcome controller renamed

--- SpringSecurityLab/src/main/java/edu/mum/waa/SpringSecurityLab/controller/HomeController.java
+++ SpringSecurityLab/src/main/java/edu/mum/waa/SpringSecurityLab/controller/HomeController.java
@@ -2,10 +2,10 @@ package edu.mum.waa.SpringSecurityLab.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+
 
 @Controller
-public class WelcomeController {
+public class HomeController {
 
     @RequestMapping("/")
     public String index() {
