b65241a233bb721726b1a33bf3b0c4ab49d913c9
986296 <segunholadapo@gmail.com>
2018-06-12 15:04:16 -0500

    JDK version changed to version 1.8

--- exercise05_3/src/main/java/cs545/bank/domain/Customer.java
+++ exercise05_3/src/main/java/cs545/bank/domain/Customer.java
@@ -1,6 +1,19 @@
 package cs545.bank.domain;
 
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.RequestScoped;
+
+
+
+@ManagedBean
+@RequestScoped
 public class Customer {
+
+	public Customer(){
+		this.name  = "Customer Class initialized here";
+		System.out.println(name);
+	}
+
 	private String name;
 
 	public Customer(String name) {
